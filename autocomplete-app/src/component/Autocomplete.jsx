5064c41dc16f49089c9473f163ae8b45a9352467
Mayank Tiwari <mayank_tiwari@epam.com>
2021-11-10 02:28:59 +0530

    Implemented Debouncing for Fetch on user input

---
+++ autocomplete-app/src/component/Autocomplete.jsx
@@ -0,0 +1,160 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import debounce from "lodash.debounce";
+
+class Autocomplete extends Component {
+  static propTypes = {
+    suggestions: PropTypes.instanceOf(Array),
+  };
+
+  static defaultProps = {
+    suggestions: [],
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // The active selection's index
+      activeSuggestion: 0,
+      // The suggestions that match the user's input
+      filteredSuggestions: [],
+      // Whether or not the suggestion list is shown
+      showSuggestions: false,
+      // What the user has entered
+      userInput: "",
+      suggestions: [],
+    };
+  }
+
+  onChange = (e) => {
+    this.debouncedInput(e.target.value);
+   
+  };
+
+  debouncedInput = debounce((text) => {
+    this.handleChangeDebounce(text)
+    this.setState({
+      activeSuggestion: 0,
+      showSuggestions: true,
+      userInput: text,
+    });
+  }, 100);
+  handleChangeDebounce = (text) => {
+      fetch("http://localhost:3004/countries?q="+text)
+        .then((res) => res.json())
+        .then((suggestions) => {
+          if (text) {
+            const userInput = text;
+            // Filter our suggestions that don't contain the user's input
+            this.setState({
+              filteredSuggestions: suggestions.filter(
+                (suggestion) =>
+                  suggestion.name.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+              ),
+            });
+          }
+        });
+    
+  };
+  onClick = (e) => {
+    this.setState({
+      activeSuggestion: 0,
+      filteredSuggestions: [],
+      showSuggestions: false,
+      userInput: e.currentTarget.innerText,
+    });
+  };
+
+  onKeyDown = (e) => {
+    const { activeSuggestion, filteredSuggestions } = this.state;
+
+    // User pressed the enter key
+    if (e.keyCode === 13) {
+      this.setState({
+        activeSuggestion: 0,
+        showSuggestions: false,
+        userInput: filteredSuggestions[activeSuggestion].name,
+      });
+    }
+    // User pressed the up arrow
+    else if (e.keyCode === 38) {
+      if (activeSuggestion === 0) {
+        return;
+      }
+
+      this.setState({ activeSuggestion: activeSuggestion - 1 });
+    }
+    // User pressed the down arrow
+    else if (e.keyCode === 40) {
+      if (activeSuggestion - 1 === filteredSuggestions.length) {
+        return;
+      }
+
+      this.setState({ activeSuggestion: activeSuggestion + 1 });
+    }
+  };
+
+  render() {
+    const {
+      onChange,
+      onClick,
+      onKeyDown,
+      state: {
+        activeSuggestion,
+        filteredSuggestions,
+        showSuggestions,
+        userInput,
+      },
+    } = this;
+
+    let suggestionsListComponent;
+
+    if (showSuggestions && userInput) {
+      if (filteredSuggestions.length) {
+        suggestionsListComponent = (
+          <ul className="suggestions">
+            {filteredSuggestions.map((suggestion, index) => {
+              let className;
+
+              // Flag the active suggestion with a class
+              if (index === activeSuggestion) {
+                className = "suggestion-active";
+              }
+
+              return (
+                <li
+                  className={className}
+                  key={suggestion.code}
+                  onClick={onClick}
+                >
+                  {suggestion.name}
+                </li>
+              );
+            })}
+          </ul>
+        );
+      } else {
+        suggestionsListComponent = (
+          <div class="no-suggestions">
+            <em>No suggestions, you're on your own!</em>
+          </div>
+        );
+      }
+    }
+
+    return (
+      <Fragment>
+        <input
+          type="text"
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={userInput}
+        />
+        {suggestionsListComponent}
+      </Fragment>
+    );
+  }
+}
+
+export default Autocomplete;
